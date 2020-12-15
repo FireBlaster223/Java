7fab21c1fb82a55fb1eaa50658f79ff3ad616ff1
FireBlaster223 <71147153+FireBlaster223@users.noreply.github.com>
2020-12-14 22:05:29 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,14 +1,14 @@
 public class App {
-    public static void main(String[] args){
-       String name = "Lucas";
-       Names a = new Names();
-       String partner = a.n1();
-       String exclaim = addExclamPoint(name);
-       System.out.println(exclaim);
-       System.out.println(partner);
-    }
-public static String addExclamPoint(String s) {
-    return s +" !";
-}
+    public static void main(String[] args) {
+        String name = "Lucas";
+        Names a = new Names();
+        String partner = a.n1();
+        String exclaim = addExclamPoint(name);
+        System.out.println(exclaim + partner);
 
- }
+    }
+
+    public static String addExclamPoint(String s) {
+        return s + " !";
+    }
+}
